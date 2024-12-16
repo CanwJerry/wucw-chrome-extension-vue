1351f11800388fceb11d581ef4ed53c0984fd993
Canwei <wu8260@163.com>
2024-12-17 00:22:03 +0800

    更新代码

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,28 @@
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+  // browser.runtime.onInstalled.addListener(async ({ reason }) => {
+  //   if (reason === 'install') {
+  //     await storage.setItem<string>("local:installDate", 'aaaaaaa');
+  //     const a = await storage.getItem<string>('local:installDate');
+  //   }
+  // });
+  console.log(window);
+  // if (window.Shopify) {
+  //   chrome.scripting.executeScript({
+  //     target: {
+  //       tabId: tab.id,
+  //     },
+  //     files: ['content-script.js'],
+  //     // func: function
+  //     world: 'MAIN'
+  //   },)
+  // } else {
+  //   chrome.notifications.create(
+  //     {
+  //       type: "basic",
+  //       title: "Notifications",
+  //       message: "This is not shopify page",
+  //       iconUrl: "../icons/candy.png"
+  //     }
+  //   );
+  // }
 });
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  extensionApi: 'chrome',
-  modules: ['@wxt-dev/module-vue'],
+  extensionApi: "chrome",
+
+  modules: ["@wxt-dev/module-vue"],
+
+  manifest: {
+    name: "Quick Entry",
+    version: "1.1.0",
+    description: "Quick entry to your shopify store",
+    permissions: ["scripting", "activeTab", "tabs", "notifications"],
+  },
 });
