d0a3fd844ecd41bef94f032a4c135495b671bfcc
canwei <wu8260@163.com>
2024-12-17 10:20:11 +0800

    移除部分代码

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,28 +1,15 @@
+const contentMatch = new MatchPattern(CONTENT_SCRIPT_MATCHES);
+
 export default defineBackground(() => {
-  // browser.runtime.onInstalled.addListener(async ({ reason }) => {
-  //   if (reason === 'install') {
-  //     await storage.setItem<string>("local:installDate", 'aaaaaaa');
-  //     const a = await storage.getItem<string>('local:installDate');
-  //   }
-  // });
-  console.log(window);
-  // if (window.Shopify) {
-  //   chrome.scripting.executeScript({
-  //     target: {
-  //       tabId: tab.id,
-  //     },
-  //     files: ['content-script.js'],
-  //     // func: function
-  //     world: 'MAIN'
-  //   },)
-  // } else {
-  //   chrome.notifications.create(
-  //     {
-  //       type: "basic",
-  //       title: "Notifications",
-  //       message: "This is not shopify page",
-  //       iconUrl: "../icons/candy.png"
-  //     }
-  //   );
-  // }
+  (browser.action ?? browser.browserAction).onClicked.addListener(
+    async (tab) => {
+      if (tab.id) {
+        const res = await browser.scripting.executeScript({
+          target: { tabId: tab.id },
+          files: ["./content.ts"],
+        });
+        console.log("result", res);
+      }
+    },
+  );
 });
