a003d88968f0b6c6223a71733983d774cc461efa
canwei <wu8260@163.com>
2024-12-17 10:00:58 +0800

    合并冲突2

--- entrypoints/popup/App.vue
+++ entrypoints/popup/App.vue
@@ -1,17 +1,9 @@
 <script lang="ts" setup>
-<<<<<<< HEAD
-import DialogBox from '@/components/DialogBox.vue';
-</script>
-
-<template>
-  <DialogBox/>
-=======
 import DialogBox from "@/components/DialogBox.vue";
 </script>
 
 <template>
   <DialogBox />
->>>>>>> 1351f11800388fceb11d581ef4ed53c0984fd993
 </template>
 
 <style scoped></style>
